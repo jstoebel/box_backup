3f35b15248ab87d81a5d22427029dd3e9cbdc2d0
Jacob Stoebel <stoebelj@berea.edu>
2016-03-31 15:52:50 -0400

    fix indentation error in main.py

--- main.py
+++ main.py
@@ -184,7 +184,7 @@ def main():
     wrapper = ClientWrapper(secrets, 'EDSdata_backup')
 
     #CHANGE THIS TO COPY WHAT EVER FOLDER YOU WANT.
-   wrapper.prep_copy(os.path.join(os.path.expanduser('~')
+    wrapper.prep_copy(os.path.join(os.path.expanduser('~')
 , 'backups/eds_backup'))
 
 if __name__ == '__main__':
